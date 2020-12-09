8b521c5e9ac2f7e936405d3f2e08841ecd32f995
Sab Saini <sab.arc@outlook.com>
2020-12-09 19:16:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/Sab-training/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-  bucket_prefix = "Sab Saini"
+  bucket_prefix = "SabSaini"
 }
